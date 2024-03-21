0d3cd48de39f8edbfb5500b2697f7acfd982bff6
Programmmeeerr <148342996+Programmmeeerr@users.noreply.github.com>
2024-03-21 21:55:34 +0900

    Add files via upload

--- js/path-change-404.js
+++ js/path-change-404.js
@@ -1,3 +1,6 @@
+var path = location.href;
+if (path.slice(-1) == '/')
+  path = path.replace(/[\/]+$/, "");
 var path2 = window.location.pathname.split("/");
 if (path2[path2.length-1] == "index")
   path = path.replace(/index$/, "");
